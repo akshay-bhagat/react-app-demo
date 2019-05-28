682971b5efa77261ba6e81502dd4b37a556d4c00
AJ <akshay.bhagat.info@gmail.com>
2019-05-28 15:32:53 +0530

    Show Details using Function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+function showDetails(character='Batman', city='Gotham'){
+    return <div>
+            <h1 className="someclass"> ⚛️ Simple React App Demo </h1>
+            <h2> {character} lives in {city} city</h2>
+        </div>
+}
+
 ReactDOM.render(
-    <h1> ⚛️ Simple React App Demo </h1> ,
+     showDetails('Iron Man', 'New York'),
     document.getElementById('root')
 );
