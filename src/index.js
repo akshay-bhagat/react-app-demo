237876c411050c1d6091cc3944fd575770e62538
AJ <akshay.bhagat.info@gmail.com>
2019-05-29 12:56:11 +0530

    Clock using States & Lifecycles

--- src/index.js
+++ src/index.js
@@ -3,16 +3,47 @@ import ReactDOM from 'react-dom'
 
 function showDetails(character='Batman', city='Gotham'){
     return <div>
-            <h1 className="someclass"> ⚛️ Simple React App Demo </h1>
+            <h1 className="someclass"> <span> ⚛️ </span> Simple React App Demo </h1>
             <h2> {character} lives in {city} city</h2>
         </div>
 }
 
-function showCurrentTime(){
-    return <h2> Current Time :: {new Date().toLocaleTimeString()}</h2>
+// function showCurrentTime(){
+//     return <h2> Current Time :: {new Date().toLocaleTimeString()}</h2>
+// }
+
+
+class ShowClock extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            date : new Date()
+        }
+    }
+
+    // calls when component is added to DOM
+    componentDidMount(){
+       this.timer = setInterval(() => this.startTime(),1000)
+    }
+
+    // calls when component is removed from DOM
+    componentWillMount(){
+        clearInterval(this.timer);
+    }
+
+    startTime(){
+        this.setState({
+            date : new Date()
+        });
+    }
+
+    render(){
+        return <h2> Current Time :: {this.state.date.toLocaleTimeString()}</h2>
+    }
 }
 
-// Component using Function
+// Component and props using Function
 function SuperHeroDetails(props){
     return <h1>Hello {props.name}, {props.city} needs you!! </h1>
 }
@@ -31,10 +62,19 @@ return <div>
     </div>
 }
 
+    // function UpdateRealTime(){
+
+    //     ReactDOM.render(
+    //          <ShowClock />,
+    //         document.getElementById('root')
+    //     );
+    // }
+    // setInterval(UpdateRealTime,1000); // refresh component every 1 second..
 
     ReactDOM.render(
-         <Show />,
-        document.getElementById('root')
-    );
+        <ShowClock />,
+       document.getElementById('root')
+   );
+
 
 
